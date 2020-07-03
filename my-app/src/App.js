68a58b8c6edcf172c93c8584d547fd62cfdd1d71
Aiya <48046501+aiya94@users.noreply.github.com>
2020-07-03 17:02:04 +0800

    remove text

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -18,16 +18,6 @@ function App() {
           </li>
         </ul>
       </header>
-      <p>
-        Avoid burnout.. Overworking yourself is a pretty common thing if you’re
-        trying to study something in your nights after work and weekends. There
-        was a point I hit where I would study and work a full day, then stay up
-        until midnight or 1am working. I remember one weekend where I’d spent
-        all day and night working on the tasks and also trying to work out, and
-        by Monday evening, I felt simply exhausted. I realized then that I had
-        probably burnt myself out. And I really needed to make time to rest...
-      </p>
-      {/* <AnimatedButton /> */}
     </div>
   );
 }
